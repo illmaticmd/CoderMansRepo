16e91dbb6336c66baf62d4a6f8db887456e51e2c
Mack <mcdesmondd@gmail.com>
2024-05-23 07:53:49 -0400

    adding new tests files

--- Learning/Python/test.py
+++ Learning/Python/test.py
@@ -11,3 +11,10 @@ print(a3 >= 21 or a1 >= 21)
 x = 'perfume'
 mall_items = ['video game','doll','clothing','not perfume', 'perfume']
 print(x in mall_items)
+
+h = 'This strin is connect', 5
+y = "yo"
+i = 5
+print(h)
+print('This strin is connect', i)
+type(y)
